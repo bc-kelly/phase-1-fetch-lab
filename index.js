3c0bfbb04eedb45365c92a02c42bf5ba7fa52787
Bridget Kelly <bckelly197@gmail.com>
2022-02-08 10:56:24 -0500

    commit tree 050b515851a937938946dea24a58e435b5a3e2bf

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch ('https://anapioficeandfire.com/api/books') 
+  return fetch ('https://anapioficeandfire.com/api/books')
+    .then (resp => resp.json())
+    .then (json => renderBooks(json))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +19,38 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+//}
+
+
+// function fetchBooks() {
+//   fetch("https://anapioficeandfire.com/api/books")
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
+//}
+
+
+
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then (res => res.json())
+//   .then ( books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   }))
+//   console.log('before fetch returns')
+// }
